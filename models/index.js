cbc2c4530ecbb6bf0c8ccb270b5837b6c569cfe0
emmya <emmy.armintrout@gmail.com>
2015-01-09 14:55:02 -0800

    trying new index.js

--- models/index.js
+++ models/index.js
@@ -3,29 +3,30 @@
 var fs        = require("fs");
 var path      = require("path");
 var Sequelize = require("sequelize");
-var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || "development";
 var config    = require(__dirname + '/../config/config.json')[env];
-var sequelize = new Sequelize(config.database, config.username, config.password, config);
 var db        = {};
+var sequelize;
+var match;
 
-  if (process.env.HEROKU_POSTGRESQL_MAUVE_URL) {
-    // the application is executed on Heroku ... use the postgres database
-    var match = process.env.HEROKU_POSTGRESQL_MAUVE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/)
+if (process.env.DATABASE_URL) {
+  match = process.env.DATABASE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/)
 
-    sequelize = new Sequelize(match[5], match[1], match[2], {
-      dialect:  'postgres',
-      protocol: 'postgres',
-      port:     match[4],
-      host:     match[3],
-      logging:  true //false
-    })
-  }
+  sequelize = new Sequelize(match[5], match[1], match[2], {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    port:     match[4],
+    host:     match[3],
+    logging:  true //false
+  });
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 fs
   .readdirSync(__dirname)
   .filter(function(file) {
-    return (file.indexOf(".") !== 0) && (file !== basename);
+    return (file.indexOf(".") !== 0) && (file !== "index.js");
   })
   .forEach(function(file) {
     var model = sequelize["import"](path.join(__dirname, file));
